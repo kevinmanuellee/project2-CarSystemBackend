c75b0eec38d1a6edc9f7b2c41168ceb55c220c24
Kevin Manuel <kevin.manueldx@hotmail.com>
2021-01-20 20:46:48 +0800

    fixing pricing service + move swagger config to config package

--- P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/config/SwaggerConfig.java
+++ P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.udacity.vehicles;
+package com.udacity.vehicles.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
